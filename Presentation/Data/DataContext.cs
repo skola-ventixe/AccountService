205d24bb8486abf37a4fb2aeba4358287fc6cbd7
Tobias Vinther <Tobias.Vinther88@gmail.com>
2025-05-14 10:24:25 +0200

    Add project files.

---
+++ Presentation/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Presentation.Data;
+
+public class DataContext(DbContextOptions<DataContext> options) : IdentityDbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+}
